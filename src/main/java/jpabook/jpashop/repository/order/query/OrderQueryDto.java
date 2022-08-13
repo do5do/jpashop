766f123267a7b849af554928ac85c98a30e8a0b2
dodo <do151015@gmail.com>
2022-08-13 20:30:48 +0900

    0813

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryDto.java
@@ -9,7 +9,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@EqualsAndHashCode(of = "orderId") // equals, hashCode 구현해주는 롬복 어노테이션 -> orderId를 기준으로 묶어준다. of는 명시적으로 특정 필드를 포함하게 하는 것.
+@EqualsAndHashCode(of = "orderId") // equals, hashCode 구현해주는 롬복 어노테이션
+// of는 명시적으로 특정 필드를 포함하게 하는 것.
+// -> orderId를 기준으로 묶어준다. -> map으로 만들 때 orderId를 기준(key)으로 grouping
 public class OrderQueryDto {
     private Long orderId;
     private String name;
